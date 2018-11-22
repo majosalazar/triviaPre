d98a44237c23de4024df538344fc470966ba6191
majosalazar <mariaj.salazarf@gmail.com>
2018-11-22 12:40:02 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+irAPreguntas = () => {
+  saludo.style.display = "none";
+  categoria.style.display = "block";
+  preguntasMundo.style.display = "none";
+  preguntasMiscelaneas.style.display = "none";
+  let name = document.getElementById("nombreIngresado").value;
+  document.getElementById("nombre").innerHTML = "Hola " + name.toUpperCase();
+}
+
+questionMundo = () => {
+  saludo.style.display = "none";
+  categoria.style.display = "none";
+  preguntasMundo.style.display = "block";
+  preguntasMiscelaneas.style.display = "none";
+}
+
+questionMiscelaneas = () => {
+  saludo.style.display = "none";
+  categoria.style.display = "none";
+  preguntasMundo.style.display = "none";
+  preguntasMiscelaneas.style.display = "block";
+}
+
+finalizar1 = () => {
+  let resultado1 = 
+}
+
+/*
+function questionmundo(){
+  var puntos = 0;
+  var respuesta1= prompt("¿Cuantos planetas tiene nuestro sistema solar?");
+  
+  if (respuesta1.toUpperCase()== "8".toUpperCase()){
+    puntos=puntos + 1;
+  }else {
+     }
+var respuesta2= prompt("¿Cuál es nuestra principal fuente de energia?");
+  if (respuesta2=='El sol')
+     {
+       puntos=puntos + 1;
+  }else {
+    }
+var respuesta3= prompt("¿Cuál es el nombre de nuestra galaxia?");
+  if (respuesta3.toUpperCase()== "la vía láctea".toUpperCase()){
+    
+    puntos=puntos + 1;
+  }else {
+      }
+  alert("tu puntaje final es " + puntos);
+ }
+
+function questionmiscelaneas(){
+  var puntos= 0;
+  var respuesta1= prompt("¿Quién descubrió America? ");
+  if (respuesta1.toUpperCase()== "Cristobal Colón".toUpperCase()){
+    
+    puntos=puntos + 1;
+  }else {
+     }
+var respuesta2= prompt("¿En qué continente se encuentra Chile?");
+  if (respuesta2.toUpperCase()== "America del sur".toUpperCase())
+     {
+    
+       puntos=puntos + 1;
+  }else {
+    }
+var respuesta3= prompt("¿En qué año terminó la segunda guerra mundial?");
+  if (respuesta3.toUpperCase()== "1945".toUpperCase()){
+    
+    puntos=puntos + 1;
+  }else {
+    }
+  alert("tu puntaje final es " + puntos);
+}
+*/ 
